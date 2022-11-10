452491cf207f867319450f59f00470e3484ab938
sadek-39 <sadeksltn@gmail.com>
2022-11-10 19:50:18 +0600

    feature/product validation added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Product\ProductStoreRequest;
+use App\Http\Requests\Product\ProductUpdateRequest;
 use App\Models\Product;
 use App\Services\ProductServices;
 use Illuminate\Http\Request;
@@ -46,7 +48,7 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductStoreRequest $request)
     {
         try {
             $productStore = $this->productServices->store($request->all());
@@ -91,7 +93,7 @@ class ProductController extends Controller
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product)
+    public function update(ProductUpdateRequest $request, Product $product)
     {
         try {
             $update = $this->productServices->update($product->id, $request->all());
